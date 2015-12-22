400962ec276ff240ef32afa5a050e49e426e1c30
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-12-22 11:40:24 -0500

    new impl using streams

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
 var http = require('http')
 var fs = require('fs')
+var linestream = require('line-stream')
+var through = require('through2')
 
-var filename = process.argv[2]
+var file = fs.createReadStream(process.argv[2])
+var ls = linestream()
+var transform = org2web()
 
-//var file = fs.readFileSync('/home/sww/Sync/athome.org').toString()
-//var topReg = new RegExp('^\\* (.*)', 'gm')
-//console.log(topReg.exec(file))
+file
+  .pipe(ls)
+  .pipe(transform)
+  .pipe(process.stdout)
 
-var res = process.stdout
-res.write('<html><meta name="viewport" content="width=device-width, initial-scale=1"><code>')
-var file = fs.readFileSync(filename).toString()
-file = file.replace(/TODO/g, '<b><font color=red>TODO</font></b>')
-file = file.replace(/DONE/g, '<b><font color=green>DONE</font></b>')
-file = file.replace(/^\* /gm, '</font><font color=blue>* ')
-file = file.replace(/^\*\* /gm, '</font><font color=green>** ')
-file = file.replace(/^\*\*\* /gm, '</font><font color=olive>*** ')
-file = file.replace(/\n/g, '<br/>')
-res.write(file)
-res.write('</code></html>')
+function org2web() {
+  var firstLine = true
+  var depth = 0
+  var transform = through(function(chunk, enc, cb) {
+    // process.stdout.write(chunk.toString())
+    if (firstLine) {
+      this.push('<html><meta name="viewport" content="width=device-width, initial-scale=1"><code>')
+      firstLine = false
+    }
+
+    var line = chunk.toString()
+    line = line.replace(/\n/g, '<br/>')
+
+    line = line.replace(/TODO/g, '<b><font color=red>TODO</font></b>')
+    line = line.replace(/DONE/g, '<b><font color=green>DONE</font></b>')
+
+    if (/^\* /.test(line)) {
+      depth = 1
+      this.push('<font color=blue>')
+    }
+    if (/^\*\* /.test(line)) {
+      depth = 2
+      this.push('<font color=green>')
+    }
+
+    this.push(line)
+
+    this.push('</font>')
+
+    cb()
+  })
+  // transform.on('end', function () {
+  //   this.push('</code></html>')
+  // })
+  return transform
+}
