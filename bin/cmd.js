138e969141cc666c6c1ecb1d80bbac0d085b77be
Stephen Whitmore <stephen.whitmore@gmail.com>
2015-12-22 15:09:59 -0500

    accept input from stdin if no filename is given

--- bin/cmd.js
+++ bin/cmd.js
@@ -3,8 +3,7 @@
 var fs = require('fs')
 var org2html = require('../index')
 
-// TODO: accept stdin if no argv[2]
-var stream = fs.createReadStream(process.argv[2])
+var stream = (process.argv.length < 3) ? process.stdin : fs.createReadStream(process.argv[2])
 
 stream
   .pipe(org2html())
